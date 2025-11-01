c7f36358fee154a8458a3376647030db62a1397f
༺ Initial H ༻ <alsyundawy@users.noreply.github.com>
2025-11-02 01:39:27 +0700

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update \
 && sudo apt install xorg \
 && sudo apt-get install firefox xvfb \
-&& wget https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz \
-&& tar -zxvf geckodriver-v0.32.0-linux64.tar.gz \
+&& wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.32.0-linux64.tar.gz \
+&& tar -zxvf geckodriver-v0.36.0-linux64.tar.gz \
 && sudo mv geckodriver /usr/bin/ \
 && sudo apt-get install default-jre \
